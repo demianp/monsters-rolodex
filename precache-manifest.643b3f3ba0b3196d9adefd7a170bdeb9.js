8f3ad6dbe1c6a0a8e078cbb4b6b7f187d2c236a1
Demian Preiti <pldemian@gmail.com>
2020-04-05 16:18:52 -0300

    Updates

---
+++ precache-manifest.643b3f3ba0b3196d9adefd7a170bdeb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d17ef0564345a7d709016afb5c2273",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "60451240b1197d5d10ca",
+    "url": "/monster-rolodex/static/css/main.da62d29e.chunk.css"
+  },
+  {
+    "revision": "cf50e43dc795e3bab6dd",
+    "url": "/monster-rolodex/static/js/2.a7ae158c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a7ae158c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60451240b1197d5d10ca",
+    "url": "/monster-rolodex/static/js/main.6683b81a.chunk.js"
+  },
+  {
+    "revision": "00f1dffcdb61402afd9d",
+    "url": "/monster-rolodex/static/js/runtime-main.54d300ab.js"
+  }
+]);
